15c9429987245746f97dc7198a6823eb7ef6a7d9
ahmedms384 <ahmedms384@gmail.com>
2024-10-31 19:25:51 -0400

    Added Google Analytics setup and .env file to .gitignore

--- src/agent-servicing-website/src/index.js
+++ src/agent-servicing-website/src/index.js
@@ -4,6 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Google Analytics Initialization
+if (process.env.REACT_APP_GOOGLE_ANALYTICS_ID) {
+  const script = document.createElement("script");
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${process.env.REACT_APP_GOOGLE_ANALYTICS_ID}`;
+  script.async = true;
+  document.head.appendChild(script);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', process.env.REACT_APP_GOOGLE_ANALYTICS_ID);
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
